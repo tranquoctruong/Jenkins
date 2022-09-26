8e32a0d3f201a26ac8eb3bd0e492661c808c5452
Tran Quoc Truong <truong.tran-quoc@banvien.com.vn>
2022-09-26 15:32:15 +0700

    init

---
+++ StringExtension/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace StringExtension
+{
+    public static class StringExtensions
+    {
+        public static int ToNumber(this string value)
+        {
+            int number = 0;
+            int.TryParse(value, out number);
+            return number;
+        }
+    }
+}
