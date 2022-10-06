01df7e941706255d1c7cbc0cb74685257643f407
Tran Quoc Truong <truong.tran-quoc@banvien.com.vn>
2022-10-06 10:26:44 +0700

    Update docker file

--- NexusWebApp/obj/Debug/net6.0/NexusWebApp.AssemblyInfo.cs
+++ NexusWebApp/obj/Debug/net6.0/NexusWebApp.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bd192efe-8688-417a-8a3e-470813f106c3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexusWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
